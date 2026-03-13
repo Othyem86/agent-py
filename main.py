2d4ad623f9f940da45ef8fd354f4f6d716995378
Othyem86 <emil.foeger@gmail.com>
2026-03-13 12:47:45 +0100

    added 'gemini-2.5-flash' model

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
-    print("Hello from agent-py!")
+    load_dotenv()
+    env_api_key = os.environ.get("GEMINI_API_KEY")
+
+    if not env_api_key:
+        raise RuntimeError("Gemini API key not found")
+
+    client = genai.Client(api_key=env_api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
