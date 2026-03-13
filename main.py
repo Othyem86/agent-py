6f9e85b47f2bfbc871cc4164e23f93a9f1539c92
Othyem86 <emil.foeger@gmail.com>
2026-03-13 12:57:29 +0100

    added prompt and response metadata output

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-def main():
+def main() -> None:
     load_dotenv()
     env_api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -14,6 +14,16 @@ def main():
         model="gemini-2.5-flash", 
         contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
     
+    print_prompt_output(response)
+
+
+def print_prompt_output(response: genai.Client) -> None:
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+    
+    print(f"Prompt tokens: {metadate.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {metadate.usage_metadata.candidates_token_count}")
+    print()
     print("Response:")
     print(response.text)
 
