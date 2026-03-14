e23ec907e99e4f8cfdecf503d4eab9712aa87c33
Othyem86 <emil.foeger@gmail.com>
2026-03-14 10:54:13 +0100

    minor refactoring

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,21 +1,13 @@
 from functions.get_files_info import get_files_info
 
-def test():
-    print(f"Result for current directory:")
-    print(get_files_info("calculator", "."))
-    print()
+def test() -> None:
+    working_dir = "calculator"
+    directories = [".", "pkg", "/bin", "../"]
 
-    print(f"Result for 'pkg' directory:")
-    print(get_files_info("calculator", "pkg"))
-    print()
-
-    print(f"Result for '/bin' directory:")
-    print(get_files_info("calculator", "/bin"))
-    print()
-
-    print(f"Result for '../' directory:")
-    print(get_files_info("calculator", "../"))
-    print()
+    for dir in directories:
+        print(f"Result for '{dir}':")
+        print(get_files_info(working_dir, dir))
+        print()
 
 if __name__ == "__main__":
     test()
